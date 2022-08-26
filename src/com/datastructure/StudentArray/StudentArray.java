49a2627c9b95acb3e7ce508b9a33211e2f1abb74
Abdelrahman Elgendy <abdelrahmanelgendy961@gmail.com>
2022-08-26 02:36:17 +0200

    This commit for Data Structure

---
+++ src/com/datastructure/StudentArray/StudentArray.java
@@ -0,0 +1,13 @@
+package com.datastructure.StudentArray;
+// Java program to illustrate creating
+//  an array of objects
+
+public class StudentArray {
+    public int roll_no;
+    public String name;
+
+    StudentArray(int roll_no, String name) {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}
